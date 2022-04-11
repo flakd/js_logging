31cd1bcb6784d995194a19ca45cb1c084378553d
Flak DiNenno <flak@dinenno.com>
2022-04-11 19:03:48 -0400

    standardized log2.submethod() calls

--- logging.js
+++ logging.js
@@ -44,7 +44,7 @@ function log2(level, callerName) {
     case 4: consoleFunc = console.debug; break;
     case 41: 
       consoleFunc = console.debug;
-      logLvlPrefix = "LLV1: ";
+      logLvlPrefix = "LLV1:";
     break;
     default:
       throw `LOGGING ERROR => 1st PARAM('${level}') is invalid. Only values [1,2,3, or 4] are valid!`
@@ -60,44 +60,39 @@ function log2(level, callerName) {
   }
 
     
-  function writeIt(loc, msg1, msg2) {
-    if (loc === undefined || loc === null) {
-      //if (msg1 ===undefined || msg2 === undefined) throw "UNDEFINED msg1 OR msg2 in 'writeIt()"
-      //if (msg1 === undefined || msg2 === undefined) {
-      //  console.error("UNDEFINED msg1 OR msg2 internal func log2.writeIt() => RETURNING");
-      //  return;
-      //} else {
-      //  // we don't add the words "Inside Of" or anything else
-        consoleFunc(`${logLvlPrefix}%c${msg1}: %c${msg2} %c.`, logStyle1, logStyle2, logStyle1);
-      //}
-    } else {
-      if (msg1 === undefined) {
+  function writeIt(loc, callrName, msg1, msg2) {
+    if (loc === undefined || loc === null) loc = "";
+    if (callrName === undefined || callrName === null) callrName = "";
+    if (msg1 === undefined || msg1 === null) msg1 = "";
+    if (msg2 === undefined || msg2 === null) msg2 = "";        
+    consoleFunc(`${logLvlPrefix} %c${loc}%c${callrName}:%c${msg1}: %c${msg2}`, 
+      logStyle1, logStyle2, logStyle1, logStyle2
+    );
+/*     else if (msg1 === undefined) {
         msg1 = "";
       } else {
         msg1 = " => " + msg1;
       }    
     }
     consoleFunc("%c%s: %c'%s()'%c%s.", logStyle1, loc, logStyle2, callerName, logStyle1, msg1);
+ */  
   };
 
 
   return ({ //  return object containing specific funcs(top, btm, next, prev)
-    w1: function w(msg1)  {
-      writeIt("", msg1,"")
+    w: function w(msg1, msg2)  {
+      writeIt("", "", msg1, msg2);
     },
-    w2: function w2(msg1, msg2) {
-      writeIt("", msg1, msg2);
+    inside: function inside(msg, msg2) {
+      writeIt("Inside of: ", callerName, msg1, msg2);
     },
-    inside: function inside(msg){
-      writeIt("Inside of: ",msg);      
-    },
-    top: function top(msg) {
+    top: function top(msg1, msg2) {
       //consoleFunc("%c                                            ", logStyle1);
       // ("%cTop of%c: '%s()'",logStyle1,logStyle2,callerName);
-      writeIt("Top of: ",msg);
+      writeIt("Top of: ", callerName, msg1, msg2);
     },
-    btm: function btm(msg) {
-      writeIt("Bottom of: ",msg);
+    btm: function btm(msg1, msg2) {
+      writeIt("Bottom of: ", callerName, msg1, msg2);
       // consoleFunc("%cBottom of%c: '%s()'",logStyle3,logStyle2,callerName);
       consoleFunc("%c--------------------------------------------", logStyle4);
     },
